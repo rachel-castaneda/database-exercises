6b75d7e63cf3148ad8fc1b54daa0acba537eda4f
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-02-24 10:47:56 -0600

    worked on order by

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,31 +1,26 @@
 use employees;
 
-#  find all employees with the first names 'irena', 'vidya', or 'maya' (709 results)
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name;
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name;
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name;
 
-# refactored the above code
 SELECT * FROM employees WHERE gender = 'M' AND first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
 
-# find all employees whose last name starts with 'e' (7330 results)
 SELECT * FROM employees WHERE last_name LIKE 'e%';
+Select * FROM employees  WHERE last_name LIKE 'e%' ORDER BY emp_no;
+Select * FROM employees  WHERE last_name LIKE 'e%' ORDER BY emp_no DESC;
 
-# refactored the above code
 SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
 SELECT * FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e';
 
-# found last names that include the letter 'e' in them but 7330 results
-
-# find all employees hired in the 90s (135214 results)
 SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
-# find all employees born on christmas (842 results)
 SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
 
-# hired in the 90s and born on christmas (362 results)
 SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%-12-25';
+SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%-12-25' ORDER BY birth_date, hire_date DESC;
 
-# find all employees with a 'q' in their last name (1873 results)
+
 SELECT * FROM employees WHERE last_name LIKE '%q%';
 
-# find all employees with a 'q' in their last name but not 'qu' (547 results)
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
