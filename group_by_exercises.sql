a9259b3bd3d3c6a58eec9507c45b54bb1f87e75b
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-02-25 09:34:47 -0600

    worked on group by

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,5 +1,21 @@
 use employees;
 
 SELECT DISTINCT title FROM titles GROUP BY title;
+# refactor using COUNT()
+SELECT COUNT(title) FROM titles GROUP BY title;
 
-SELECT DISTINCT first_name last_name FROM employees WHERE first_name OR last_name LIKE 'e%e' ORDER BY last_name;
+SELECT DISTINCT last_name FROM employees WHERE last_name LIKE 'e%e' GROUP BY last_name;
+# refactor using COUNT()
+SELECT COUNT(last_name) FROM employees WHERE last_name LIKE 'e%e' ORDER BY last_name;
+
+
+SELECT DISTINCT first_name,last_name FROM employees WHERE first_name OR last_name LIKE 'e%e' GROUP BY first_name, last_name;
+# refactor using COUNT()
+SELECT COUNT(first_name and last_name)FROM employees WHERE first_name OR last_name LIKE 'e%e' ORDER BY first_name, last_name;
+
+
+SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%' GROUP BY last_name;
+# refactor using COUNT()
+SELECT COUNT(last_name) FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%' ORDER BY last_name;
+
+SELECT gender, count(*) FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')  group by gender;
